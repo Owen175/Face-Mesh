74c063b45b25cfbe0fe3b0a3191f66f9afd9628f
Owen175 <oclocliphone@icloud.com>
2022-12-03 15:49:25 +0000

    Includes Drawing specifications - edits

--- FaceMeshBasics.py
+++ FaceMeshBasics.py
@@ -8,7 +8,7 @@ pTime = 0
 mpDraw = mp.solutions.drawing_utils
 mpFaceMesh = mp.solutions.face_mesh
 faceMesh = mpFaceMesh.FaceMesh(max_num_faces=2)
-
+drawSpec = mpDraw.DrawingSpec(thickness=1, circle_radius=1)
 
 while True:
     success, img = cap.read()
@@ -17,7 +17,7 @@ while True:
 
     if results.multi_face_landmarks:
         for faceLms in results.multi_face_landmarks:
-            mpDraw.draw_landmarks(img, faceLms, mpFaceMesh.FACEMESH_CONTOURS)
+            mpDraw.draw_landmarks(img, faceLms, mpFaceMesh.FACEMESH_CONTOURS, drawSpec, drawSpec)
 
     cTime = time.time()
     fps = 1/(cTime - pTime)
